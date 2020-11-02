94a1f79a97144bab6d8f257bdf084112a40129d5
HauresBogdan <hauresbogdan@gmail.com>
2020-11-02 21:22:16 +0200

    vue practive1

--- Vue practive/Vue CLI/vue1/src/App.vue
+++ Vue practive/Vue CLI/vue1/src/App.vue
@@ -1,26 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Dota Heroes {{ heroesCount }}</h1>
+
+  <ul>
+    <li v-for="hero in dotaList" :key="hero.name">{{ hero.name }}</li>
+  </ul>
+
+  <h2>{{ fName }}{{ lName }}</h2>
+  <h2>{{ fullName }}</h2>
+  <p>{{ RandA }}</p>
+  <p>{{ RandA }}</p>
+  <p>{{ RandA }}</p>
+  <p>{{ RandB }}</p>
+  <p>{{ RandB }}</p>
+  <p>{{ RandCMethod() }}</p>
+  <p>{{ RandCMethod() }}</p>
+  <p>{{ RandCMethod() }}</p>
+  <button @click="setFullName">Set fullName</button>
+
+  <form action="submit" @submit.prevent="addHeroFunction">
+    <input type="text" v-model="addHero" placeholder="add hero" />
+    <button type="submit">Add Hero</button>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      fName: "Haures",
+      lName: "Bogdan",
+      addHero: "",
+      dotaList: [{ name: "Cristal Maiden" }, { name: "Pudge" }, { name: "Phantom  Assasin" }],
+    };
+  },
+  methods: {
+    addHeroFunction() {
+      if (this.addHero !== "") {
+        this.dotaList.unshift({ name: this.addHero });
+        this.addHero = "";
+      }
+    },
+    setFullName() {
+      this.fullName = 'mamamia';
+    },
+    RandCMethod() {
+      return Math.random();
+    },
+  },
+  computed: {
+    heroesCount() {
+      return this.dotaList.length;
+    },
+    RandA() {
+      return Math.random();
+    },
+    RandB() {
+      return Math.random();
+    },
+    fullName: {
+      get() {
+        return `${this.fName} ${this.lName}`;
+      },
+      set(fullName) {
+        const nameArr = fullName.split(" ");
+        this.fName = nameArr[0];
+        this.lName = nameArr[1];
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
